158523642520670385430fba3e6728d780e591d8
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-10-24 10:02:00 +0200

    Reorganize the inputs and outputs

--- run/analysis/predict.m
+++ run/analysis/predict.m
@@ -1,5 +1,5 @@
-function example()
-  values = h5read('example.h5', '/values');
+function predict(file)
+  values = h5read(file, '/values');
   assert(size(values, 2) == 1);
   histogram(values, 100, 'Normalization', 'pdf');
   title('Empirical PDF');
